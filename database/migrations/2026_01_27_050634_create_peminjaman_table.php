802f81e2a7b20c0762dff58857ef605608d32773
akangHees-spec <kangobel74@gmail.com>
2026-01-27 13:39:53 +0700

    initial commit: sistem peminjaman alat

---
+++ database/migrations/2026_01_27_050634_create_peminjaman_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('peminjaman', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->date('tanggal_pinjam');
+            $table->date('tanggal_kembali_rencana');
+            $table->enum('status', ['dipinjam', 'dikembalikan', 'terlambat']);
+            $table->string('keperluan');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('peminjaman');
+    }
+};
